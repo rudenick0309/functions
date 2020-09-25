3e9ced3a5a487b2b4cf6dc974a64f0feb79a0b6c
rudenick0309 <rudenick0309@gmail.com>
2020-09-26 00:29:46 +0900

    Feat: for infinity scrolling, added faker data to 'name', 'age', 'img'. At now, there are ten of data.

--- pages/_app.js
+++ pages/_app.js
@@ -3,11 +3,12 @@ import Head from 'next/head';
 import {loadJsonRequest} from "../reducers/infinity";
 import {useDispatch, useSelector} from "react-redux";
 import wrapper from '../store/configureStore'
+import Infinity from "../components/infinity";
 
 const Functions = ({ Component }) => {
   const dispatch = useDispatch();
-  const data = useSelector((state) => state.loadJsonBucket)
-  console.log('f -app, data;',data);
+  const infinityData = useSelector((state) => state.infinity?.loadJsonBucket)
+  console.log('f -app, data;',infinityData);
 
   const onClickInfinity = useCallback(() => {
     console.log('인피니티 작동함')
@@ -17,6 +18,8 @@ const Functions = ({ Component }) => {
   const onClickVirtualized = useCallback(() => {
     console.log('버츄얼라이즈드 작동함')
   }, [])
+
+
 
   return (
     <>
@@ -29,6 +32,10 @@ const Functions = ({ Component }) => {
         <span onClick={onClickInfinity}>버튼1</span>
         <span onClick={onClickVirtualized}>버튼2</span>
       </div>
+
+      {infinityData && infinityData.map((el) => {
+        return <Infinity key={el.id} data={el} />
+      })}
 
       <Component />
 
