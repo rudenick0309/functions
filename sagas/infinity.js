7a9653090b689aea8df19c426e33771565347c5c
rudenick0309 <rudenick0309@gmail.com>
2020-09-26 00:30:27 +0900

    Feat: for infinity scrolling, added faker data to 'name', 'age', 'img'. At now, there are ten of data.

--- sagas/infinity.js
+++ sagas/infinity.js
@@ -5,16 +5,16 @@ import {LOAD_JSON_FAILURE, LOAD_JSON_REQUEST, LOAD_JSON_SUCCESS} from "../reduce
 
 
 function loadJsonAPI() {
-  console.log('f saga 4 loadjson api')
-  return axios.get('/ahshes')
+  // console.log('f saga 4 loadjson api')
+  return axios.get('/test')
 }
 
 function* loadJson() {
-  console.log('f saga 3 loadjson')
+  // console.log('f saga 3 loadjson')
   try {
     const result = yield call(loadJsonAPI);
-    yield delay(2000);
-    console.log('f saga 3 infinity result; ',result);
+    yield delay(1000);
+
     yield put({
       type:LOAD_JSON_SUCCESS,
       data:result.data,
@@ -29,12 +29,12 @@ function* loadJson() {
 }
 
 function* watchInfinity() {
-  console.log('f saga 2 watchinfinity')
+  // console.log('f saga 2 watchinfinity')
   yield takeLatest(LOAD_JSON_REQUEST, loadJson);
 }
 
 export default function* infinitySaga() {
-  console.log('f saga 1 infinitysaga')
-  // yield all[fork(watchInfinity)]
+  // console.log('f saga 1 infinitysaga')
+  // yield all[fork(watchInfinity)]   // 하나일 때 all 하면 안 되더라 실행 자체가 안 됐음
   yield fork(watchInfinity)
 }
