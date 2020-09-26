34b11b5c09b20ad7f1abd615a8a82213d486b15e
rudenick0309 <rudenick0309@gmail.com>
2020-09-26 12:45:25 +0900

    Feat: infinity scrolling finished.

--- pages/_app.js
+++ pages/_app.js
@@ -6,18 +6,8 @@ import wrapper from '../store/configureStore'
 import Infinity from "../components/infinity";
 
 const Functions = ({ Component }) => {
-  const dispatch = useDispatch();
-  const infinityData = useSelector((state) => state.infinity?.loadJsonBucket)
-  console.log('f -app, data;',infinityData);
 
-  const onClickInfinity = useCallback(() => {
-    console.log('인피니티 작동함')
-    dispatch(loadJsonRequest())
-  }, [])
 
-  const onClickVirtualized = useCallback(() => {
-    console.log('버츄얼라이즈드 작동함')
-  }, [])
 
 
 
@@ -28,14 +18,7 @@ const Functions = ({ Component }) => {
         <title>Functions</title>
       </Head>
 
-      <div>
-        <span onClick={onClickInfinity}>버튼1</span>
-        <span onClick={onClickVirtualized}>버튼2</span>
-      </div>
 
-      {infinityData && infinityData.map((el) => {
-        return <Infinity key={el.id} data={el} />
-      })}
 
       <Component />
 
