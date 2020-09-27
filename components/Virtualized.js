db8b6828153cd874963d1c84d593e86f209c47c2
rudenick0309 <rudenick0309@gmail.com>
2020-09-28 00:02:55 +0900

    Feat: pagination & react virtualized & normal component. By using 9,000 of fake data

---
+++ components/Virtualized.js
@@ -0,0 +1,94 @@
+import React, {useEffect, useRef, useState} from "react";
+import {List, AutoSizer, CellMeasurer, CellMeasurerCache} from "react-virtualized";
+import faker from "faker";
+
+// export default function Virtualized() {
+const Virtualized = () => {
+  const [people, setPeople] = useState([]);
+
+  const [time, setTime] = useState(new Date());
+  const cache = useRef(new CellMeasurerCache({
+    fixedWidth: true,
+    defaultHeight: 100,
+  }));
+
+
+  useEffect(() => {
+    setPeople(
+      [...Array(3000).keys()].map((key) => {
+        return {
+          id: key,
+          name: `${faker.name.firstName()} ${faker.name.lastName()}`,
+          images: faker.image.image()
+        };
+      })
+    );
+  }, []);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setTime(new Date());
+    }, 1000);
+
+    return () => clearInterval(interval);
+  }, []);
+
+  return (
+    <div>
+
+      <div style={styles.wrapper}>
+        <AutoSizer>
+          {({width, height}) => (
+            <List
+              width={width}
+              height={height}
+              rowHeight={cache.current.rowHeight}
+              deferredMeasurementCache={cache.current}
+              rowCount={people.length}
+              rowRenderer={({key, index, style, parent}) => {
+                const person = people[index];
+
+                return (
+                  <CellMeasurer
+                    key={key}
+                    cache={cache.current}
+                    parent={parent}
+                    columnIndex={0}
+                    rowIndex={index}
+                  >
+                    <div style={style}>
+                      <h2>{person.name}</h2>
+                      <img src={person.images} />
+                    </div>
+                  </CellMeasurer>
+                );
+              }}
+            />
+            )}
+        </AutoSizer>
+      </div>
+
+    </div>
+
+    //
+
+  );
+}
+
+const styles = {
+  wrapper: {
+    width: "50%",
+    height: "100vh",
+    // position:'relative',
+    left:'30%',
+
+  },
+  font:{
+    color:'white',
+  },
+
+};
+
+export default Virtualized;
+
+
