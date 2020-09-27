ca796086cd9853960ab696699f03c3c9e4d557c2
rudenick0309 <rudenick0309@gmail.com>
2020-09-28 00:01:16 +0900

    Feat: pagination & react virtualized & normal component. By using 9,000 of fake data

--- reducers/index.js
+++ reducers/index.js
@@ -1,6 +1,9 @@
 import {HYDRATE} from "next-redux-wrapper";
 import {combineReducers} from "redux";
+
 import infinity from './infinity';
+import normal from './normal'
+import pagination from './pagination'
 
 const rootReducer = (state, action) => {
   switch (action.type) {
@@ -10,6 +13,8 @@ const rootReducer = (state, action) => {
     default : {
       const combinedReducer = combineReducers({
         infinity,
+        normal,
+        pagination,
       });
       return combinedReducer(state, action);
     }
