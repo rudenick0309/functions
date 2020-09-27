d95c6fe9cefc51474416c28ffab1712f18fe4f62
rudenick0309 <rudenick0309@gmail.com>
2020-09-28 00:44:06 +0900

    Feat: pagination & react virtualized & normal component. By using 9,000 of fake data

--- components/PaginationTwo.js
+++ components/PaginationTwo.js
@@ -1,38 +1,39 @@
-import React from 'react';
+import React from "react";
 
-const PaginationTwo = ({ postsPerPage, totalPosts , paginate}) => {
+const PaginationTwo = ({postsPerPage, totalPosts, paginate}) => {
   const pageNumbers = [];
 
-  for(let i = 1; i <= Math.ceil(totalPosts / postsPerPage); i +=1) {
+  for (let i = 1; i <= Math.ceil(totalPosts / postsPerPage); i += 1) {
     pageNumbers.push(i);
   }
 
   return (
-    <nav>
-      <div style={styles.wrapper} className={'pagination'}>
-        {pageNumbers.map(number =>
-          <div style={styles.content} key={number} className={'page-item'}>
-            <a onClick={() => paginate(number)}  className={"page-link"}>
+    <div style={styles.wrapper}>
+      {pageNumbers.map(number =>
+        <div  className={"pagination"} onClick={() => paginate(number)}>
+          <div style={styles.content} key={number} className={"page-item"}>
+            <a className={"page-link"}>
               {number}
             </a>
 
           </div>
-        )}
-      </div>
-    </nav>
-  )
-}
+        </div>
+      )}
+    </div>
+  );
+};
 
 const styles = {
-  wrapper:{
-    display:'flex',
+  wrapper: {
+    display: "flex",
+    border:'2px solid red',
   },
   content: {
-    marginRight:'10px',
-    padding:'20px',
-    border:'3px solid grey',
-    borderRadius:'20px',
+    marginRight: "10px",
+    padding: "20px",
+    border: "3px solid grey",
+    borderRadius: "20px",
   }
-}
+};
 
 export default PaginationTwo;
