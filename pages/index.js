9a025309da5139b01f98e21876aa7f51becb2050
rudenick0309 <rudenick0309@gmail.com>
2020-09-28 00:01:09 +0900

    Feat: pagination & react virtualized & normal component. By using 9,000 of fake data

--- pages/index.js
+++ pages/index.js
@@ -1,66 +1,94 @@
-import React, {useCallback, useEffect} from "react";
+import React, {useCallback, useEffect, useState} from "react";
 import {useDispatch, useSelector} from "react-redux";
-import {LOAD_INFINITY_REQUEST, loadInfinityRequest} from "../reducers/infinity";
-import Infinity from "../components/infinity";
-import wrapper from '../store/configureStore'
-
+import wrapper from "../store/configureStore";
+import NormalExecute from "../components/NormalExecute";
+import InfinityExecute from "../components/infinityExecute";
+import VirtualizedExecute from "../components/VirtualizedExecute";
+import PaginationExecute from "../components/PaginationExecute";
 
 
 const Home = () => {
-  console.log('홈')
-  const dispatch = useDispatch();
-  const infinityBucket = useSelector((state) => state.infinity?.InfinityBucket)
-  const hasMoreBucket = useSelector((state) => state.infinity?.hasMoreBucket)
-  const loadInfinityLoading = useSelector((state) => state.infinity?.loadInfinityLoading)
+  console.log("홈");
+  const [normal, setNormal] = useState(false);
+  const [infinity, setInfinity] = useState(false);
+  const [virtualized, setVirtualized] = useState(false);
+  const [pagination, setPagination] = useState(false);
+
+  const onClickNormal = useCallback(() => {
+    setNormal(true);
+    setInfinity(false);
+    setVirtualized(false);
+    setPagination(false);
+  }, []);
 
   const onClickInfinity = useCallback(() => {
-    console.log('인피니티 작동함')
+    console.log("인피니티 작동함");
     // dispatch(loadInfinityRequest())
-  }, [])
+    setNormal(false);
+    setInfinity(true);
+    setVirtualized(false);
+    setPagination(false);
+  }, []);
 
   const onClickVirtualized = useCallback(() => {
-    console.log('버츄얼라이즈드 작동함')
-  }, [])
+    console.log("버츄얼라이즈드 작동함");
+    setNormal(false);
+    setInfinity(false);
+    setVirtualized(true);
+    setPagination(false);
+  }, []);
 
-  useEffect(() => {
-    dispatch({
-      type: LOAD_INFINITY_REQUEST,
-    })
-  },[])
+  const onClickPagination = useCallback(() => {
+    console.log("페이지네이션 작동함");
+    setNormal(false);
+    setInfinity(false);
+    setVirtualized(false);
+    setPagination(true);
+  }, []);
 
-  useEffect(() => {    // useEffcet에서 윈도우 이벤트를 쓸 떄는 항상 return으로 해당 함수를 clear해줘야 한다.
-    function onScroll () {
-      console.log(window.scrollY, document.documentElement.clientHeight, document.documentElement.scrollHeight)
-      if (window.scrollY + document.documentElement.clientHeight > document.documentElement.scrollHeight - 2400 ) {
-        if (hasMoreBucket && !loadInfinityLoading) {
-          dispatch({
-            type:LOAD_INFINITY_REQUEST,
-          })
-        }
-      }
-    }
-      window.addEventListener('scroll', onScroll)
-    return () => {
-      window.removeEventListener('scroll', onScroll);
-    }
-
-
-  },[hasMoreBucket, loadInfinityLoading])
 
   return (
     <>
       <div>
+        <span onClick={onClickNormal}>Normal scrolling    </span>
         <span onClick={onClickInfinity}>Infinity Scrolling    </span>
-        <span onClick={onClickVirtualized}>React virtualized</span>
+        <span onClick={onClickVirtualized}>React virtualized    </span>
+        <span onClick={onClickPagination}>Pagination     </span>
       </div>
 
+      {normal
+        ? (<NormalExecute/>)
+        : (<></>)
+      }
 
-      {infinityBucket && infinityBucket.map((el) => {
-        return <Infinity key={el.id} data={el} />
-      })}
+      {infinity
+        ? (<InfinityExecute/>)
+        : (<></>)
+      }
+
+      {virtualized
+        ? (<VirtualizedExecute/>)
+        : (<></>)
+      }
+
+      {pagination
+        ? (<PaginationExecute/>)
+        : (<></>)
+      }
     </>
 
-  )
-}
+  );
+};
+
+const styles = {
+  fixed: {
+    position: "fixed",
+  },
+
+  wrapper: {
+    padding: "130px",
+    border: "2px solid red",
+  },
+};
 
 export default wrapper.withRedux(Home);
