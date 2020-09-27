9d9cbed2ec79c6b9cda62587df94576c9802697b
rudenick0309 <rudenick0309@gmail.com>
2020-09-28 00:02:59 +0900

    Feat: pagination & react virtualized & normal component. By using 9,000 of fake data

---
+++ components/VirtualizedExecute.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Virtualized from './Virtualized'
+
+const VirtualizedExecute = () => {
+  return (
+    <div>
+
+      <Virtualized />
+
+      </div>
+  )
+}
+
+export default VirtualizedExecute;
