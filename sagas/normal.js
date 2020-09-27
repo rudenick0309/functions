38652f7dca7de31be4f56e81991374cd4e0a5bb8
rudenick0309 <rudenick0309@gmail.com>
2020-09-28 00:02:07 +0900

    Feat: pagination & react virtualized & normal component. By using 9,000 of fake data

---
+++ sagas/normal.js
@@ -0,0 +1,43 @@
+import {delay, put, call, takeLatest, fork} from "redux-saga/effects";
+import {LOAD_NORMAL_FAILURE, LOAD_NORMAL_REQUEST, LOAD_NORMAL_SUCCESS} from "../reducers/normal";
+import axios from "axios";
+
+
+
+function loadNormalAPI() {
+  console.log('f saga 4 loadNormalAPI')
+  return axios.get('/normal')
+}
+
+function* loadNormal() {
+  console.log('f saga 3 loadNormal')
+  try {
+    const result = yield call(loadNormalAPI);
+    // yield delay(1000);
+    console.log('f saga 3 loadNormal result,; ', result);
+    yield put({
+      type:LOAD_NORMAL_SUCCESS,
+      // data:result.data,
+      data:result.data,
+    })
+  } catch(error) {
+    console.error(error);
+    yield put({
+      type:LOAD_NORMAL_FAILURE,
+      error:error.response.data,
+    })
+  }
+}
+
+
+function* watchNormal() {
+  console.log('f saga 2 watchnormal')
+  yield takeLatest(LOAD_NORMAL_REQUEST, loadNormal);
+}
+
+
+export default function* normalSaga() {
+  console.log('f saga 1 normalsaga')
+
+  yield fork(watchNormal);
+}
