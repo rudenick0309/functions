495c3f44d00a4ef09384464b7f52ecbd8d0c7b38
rudenick0309 <rudenick0309@gmail.com>
2020-09-28 00:01:20 +0900

    Feat: pagination & react virtualized & normal component. By using 9,000 of fake data

--- sagas/index.js
+++ sagas/index.js
@@ -1,11 +1,18 @@
-import axios from 'axios'
-import {all, fork } from "redux-saga/effects";
+import axios from "axios";
+import {all, fork} from "redux-saga/effects";
 
 import infinitySaga from "./infinity";
+import normalSaga from "./normal";
+import paginationSaga from "./pagination";
 
-axios.defaults.baseURL = 'http://localhost:4000';
+axios.defaults.baseURL = "http://localhost:4000";
 
 export default function* rootSaga() {
   // console.log('saga index')
-  yield fork(infinitySaga)
+  // yield fork(infinitySaga)
+  yield all([
+    fork(infinitySaga),
+    fork(normalSaga),
+    fork(paginationSaga),
+  ]);
 }
