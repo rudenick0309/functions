ac76ecb462b0178f31049815f0a8c03b999a969a
rudenick0309 <rudenick0309@gmail.com>
2020-09-28 00:01:52 +0900

    Feat: pagination & react virtualized & normal component. By using 9,000 of fake data

---
+++ components/Normal.js
@@ -0,0 +1,31 @@
+import React from 'react';
+
+const Normal = ({data}) => {
+  console.log('f c normal data;', data)
+  const { name, image  } = data;
+
+  return (
+    <div style={styles.wrapper}>
+      <div>
+        {name}
+      </div>
+      <div>
+        <img src={image}/>
+      </div>
+    </div>
+  )
+}
+
+const styles = {
+  wrapper: {
+    padding:'20px',
+    display:'flex',
+    flexDirection:'column',
+    justifyContent:'center',
+    alignItems:'center',
+    marginBottom:'40px',
+  }
+}
+
+
+export default Normal;
