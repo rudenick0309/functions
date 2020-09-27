36b8d161ad6d82cb9ed55b6e6e1b490422efdb66
rudenick0309 <rudenick0309@gmail.com>
2020-09-28 00:02:00 +0900

    Feat: pagination & react virtualized & normal component. By using 9,000 of fake data

---
+++ reducers/normal.js
@@ -0,0 +1,43 @@
+import produce from "immer";
+import shortid from 'shortid';
+import faker from 'faker';
+
+export const initialState = {
+  normalBucket : null,
+  loadNormalLoading: false,
+  loadNormalDone: false,
+  loadNormalError: null,
+}
+
+
+
+export const LOAD_NORMAL_REQUEST = "LOAD_NORMAL_REQUEST"
+export const LOAD_NORMAL_SUCCESS = "LOAD_NORMAL_SUCCESS"
+export const LOAD_NORMAL_FAILURE = "LOAD_NORMAL_FAILURE"
+
+const reducer = (state=initialState, action) => produce(state, (draft) => {
+  switch (action.type) {
+    case LOAD_NORMAL_REQUEST:
+      console.log("f reducer infinity, load infinity request,  executes");
+      draft.loadNormalLoading = true;
+      draft.loadNormalDone = false;
+      draft.loadNormalError = null;
+      break;
+    case LOAD_NORMAL_SUCCESS:
+      console.log("f reducer infinity, load infinity success, action; ", action);
+      draft.loadNormalLoading = false;
+      draft.loadNormalDone = true;
+      // draft.NormalBucket = draft.NormalBucket.concat(action.data);
+      draft.normalBucket = action.data;
+      break;
+    case LOAD_NORMAL_FAILURE:
+      console.log("f reducer infinity, load infinity failure, action; ", action);
+      draft.loadInfinityLoading = false;
+      draft.loadInfinityError = action.error;
+      break;
+    default:
+      break;
+  }
+})
+
+export default reducer;
