076721ba92efc946a279de305051566e9184afa1
rudenick0309 <rudenick0309@gmail.com>
2020-09-28 00:02:13 +0900

    Feat: pagination & react virtualized & normal component. By using 9,000 of fake data

---
+++ components/NormalExecute.js
@@ -0,0 +1,34 @@
+import React, {useEffect,useState} from 'react';
+import Normal from './Normal'
+import {LOAD_NORMAL_REQUEST} from "../reducers/normal";
+import {useDispatch, useSelector} from "react-redux";
+
+const NormalExecute = () => {
+  const dispatch = useDispatch();
+  const normalBucket = useSelector((state) => state.normal?.normalBucket)
+
+  console.log('f c, normalbucket;', normalBucket)
+
+  useEffect(() => {
+    dispatch({
+      type:LOAD_NORMAL_REQUEST,
+    })
+  }, [])
+
+  return (
+    <div style={styles.wrapper}>
+
+      {normalBucket?.map((el) => {
+        return <Normal data={el}/>
+      })}
+
+    </div>
+  )
+}
+
+const styles = {
+  wrapper:{
+  }
+}
+
+export default NormalExecute;
