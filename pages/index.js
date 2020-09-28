55149a0d2840408d0dab4605e123a9a1e7e2a9b2
rudenick0309 <rudenick0309@gmail.com>
2020-09-28 09:06:15 +0900

    Fix: pagination modified to style to border color. And the image of the Virtualized has downsized to 400px.

--- pages/index.js
+++ pages/index.js
@@ -9,7 +9,7 @@ import PaginationExecute from "../components/PaginationExecute";
 
 const Home = () => {
   console.log("홈");
-  const [home, setHome] = useState(false);
+  const [home, setHome] = useState(true);
   const [normal, setNormal] = useState(false);
   const [infinity, setInfinity] = useState(false);
   const [virtualized, setVirtualized] = useState(false);
@@ -70,14 +70,18 @@ const Home = () => {
         <span onClick={onClickPagination}>Pagination     </span>
       </div>
 
-      <div>
-        devsner의 기능 연습장입니다.
-        <br />
-        현재 추가된 기능은,
-        1. Normal scrolling -> 더미 데이터가 9,000개입니다. 초기 로딩 속도가 5초 정도 걸릴 수 있습니다.
-        2. Infinity scrolling -> 모니터의 높이에 따라 새로운 데이터를 계속해서 불러들입니다. 재요청하는 시점은, 현재 출력되고 있는 데이터 개수에 따라, 전체 높이의 3분의 2 정도 되는 지점에서 데이터를 불러들입니다
-        3. React virtualized ->
-      </div>
+      {home
+      ? (<div>
+          devsner의 기능 연습장입니다.
+          <br />
+          현재 추가된 기능은,
+          1. Normal scrolling -> 더미 데이터가 9,000개입니다. 초기 로딩 속도가 5초 정도 걸릴 수 있습니다.
+          2. Infinity scrolling -> 모니터의 높이에 따라 새로운 데이터를 계속해서 불러들입니다. 재요청하는 시점은, 현재 출력되고 있는 데이터 개수에 따라, 전체 높이의 3분의 2 정도 되는 지점에서 데이터를 불러들입니다
+          3. React virtualized ->
+        </div>)
+      : (<></>)
+      }
+
 
       {normal
         ? (<NormalExecute/>)
