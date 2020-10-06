d408773561a3b2d46dba8a4c98d33da5c4f64f8a
rudenick0309 <rudenick0309@gmail.com>
2020-10-06 10:58:50 +0900

    Feat: for deploy, deleted unused console.log

--- reducers/normal.js
+++ reducers/normal.js
@@ -18,20 +18,20 @@ export const LOAD_NORMAL_FAILURE = "LOAD_NORMAL_FAILURE"
 const reducer = (state=initialState, action) => produce(state, (draft) => {
   switch (action.type) {
     case LOAD_NORMAL_REQUEST:
-      console.log("f reducer infinity, load infinity request,  executes");
+      // console.log("f reducer infinity, load infinity request,  executes");
       draft.loadNormalLoading = true;
       draft.loadNormalDone = false;
       draft.loadNormalError = null;
       break;
     case LOAD_NORMAL_SUCCESS:
-      console.log("f reducer infinity, load infinity success, action; ", action);
+      // console.log("f reducer infinity, load infinity success, action; ", action);
       draft.loadNormalLoading = false;
       draft.loadNormalDone = true;
       // draft.NormalBucket = draft.NormalBucket.concat(action.data);
       draft.normalBucket = action.data;
       break;
     case LOAD_NORMAL_FAILURE:
-      console.log("f reducer infinity, load infinity failure, action; ", action);
+      // console.log("f reducer infinity, load infinity failure, action; ", action);
       draft.loadInfinityLoading = false;
       draft.loadInfinityError = action.error;
       break;
