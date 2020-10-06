ead1ce01961f994b82d4e469d8b762f9107a3e2a
rudenick0309 <rudenick0309@gmail.com>
2020-10-06 10:37:14 +0900

    Feat: for deploy, deleted unused console.log and package, login

--- sagas/infinity.js
+++ sagas/infinity.js
@@ -12,12 +12,12 @@ import {LOAD_NORMAL_REQUEST} from "../reducers/normal";
 
 
 function loadInfinityAPI() {
-  console.log('f saga 4 loadinfinity api')
+  // console.log('f saga 4 loadinfinity api')
   return axios.get('/test')
 }
 
 function* loadInfinity() {
-  console.log('f saga 3 loadinfinity')
+  // console.log('f saga 3 loadinfinity')
   try {
     // const result = yield call(loadInfinityAPI);
     yield delay(1000);
@@ -38,13 +38,13 @@ function* loadInfinity() {
 
 
 function* watchInfinity() {
-  console.log('f saga 2 watchinfinity')
+  // console.log('f saga 2 watchinfinity')
   yield takeLatest(LOAD_INFINITY_REQUEST, loadInfinity);
   // yield throttle(10000, LOAD_INFINITY_REQUEST, loadInfinity);  // 요청을 한 번만 보낼 수 있는 방법을 프론트에서 생각해야 한다.
 }
 
 export default function* infinitySaga() {
-  console.log('f saga 1 infinitysaga')
+  // console.log('f saga 1 infinitysaga')
   yield fork(watchInfinity)   // 하나일 때 all 하면 안 되더라 실행 자체가 안 됐음
   // yield fork(watchInfinity)
 }
