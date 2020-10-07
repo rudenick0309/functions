87aebe308e55a2a735487ea36f0c3910e24786fc
rudenick0309 <rudenick0309@gmail.com>
2020-10-07 23:23:59 +0900

    Style: delete unused console

--- components/Pagination.js
+++ components/Pagination.js
@@ -1,13 +1,6 @@
 import React from 'react';
 
-
 const Pagination = ({ posts, loading }) => {
-  // console.log('f c pagination posts;', posts)
-  // console.log('f c pagination loading;', loading)
-  // if(loading) {
-  //   return <h2>Loading...</h2>
-  // }
-
   return (
     <ul >
       {posts?.map(post =>(
