bdeed39567ce597b0ee83487d93d4d446d1f5f37
rudenick0309 <rudenick0309@gmail.com>
2020-10-10 22:13:50 +0900

    Feat: added changed css mode

---
+++ components/button.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Button = ({title, click}) => {
+  return (
+    // <S.Button onClick={click}>
+    <Buttons onClick={click}>
+      <span>{title}</span>
+    </Buttons>
+  );
+};
+
+export default Button;
+
+const Buttons = styled.button`
+  border:20px solid yellow;
+
+  margin: 5rem;
+  width:20rem;
+  height: 20rem;
+  background-color: #6e827f;
+  color: ${props => props.theme.colors.titleColor}; 
+  border-radius: 8px;
+  cursor: pointer;
+  
+  span{
+    font-size: 1.6rem;
+    font-weight: bold;
+  }
+`;
