031290eae331166831d57c01257acd0ef8248fcc
rudenick0309 <rudenick0309@gmail.com>
2020-10-10 22:20:11 +0900

    Feat: added changed css mode

---
+++ components/theme.js
@@ -0,0 +1,13 @@
+export const dark ={
+  colors:{
+    titleColor : '#121212',
+    bgColor: '#2a0082',
+  }
+}
+
+export const light ={
+  colors:{
+    titleColor : '#b8b8b8',
+    bgColor: 'navy',
+  }
+}
